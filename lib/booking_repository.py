69349ebb7329b9dd183a7fd1e31d0932a00c9559
zubayda <76794463+Zhagi@users.noreply.github.com>
2023-08-30 15:36:12 +0100

    booking repo done

--- lib/booking_repository.py
+++ lib/booking_repository.py
@@ -22,5 +22,5 @@ class BookingRepository():
         return Booking(row['id'], row["start_date"], row["end_date"], row["user_id"], row["space_id"])
     
     def create(self, booking):
-        self.connection.execute("INSERT INTO bookings(id, start_date, end_date, user_id, space_id) VALUES(%s, %s, %s, %s, %s)", [booking.id, booking.start_date, booking.end_date, booking.user_id, booking.space_id])
-        return None
+        self.connection.execute("INSERT INTO bookings(start_date, end_date, user_id, space_id) VALUES(%s, %s, %s, %s)", [booking.start_date, booking.end_date, booking.user_id, booking.space_id])
+        return booking.id
