c023eb6783a5a14f8dd7e1565651fbf13f13d23d
sarahellis <sarahellislondon@gmail.com>
2023-08-31 11:40:32 +0100

    Space and SpaceRepository classes and tests complete

--- lib/space.py
+++ lib/space.py
@@ -14,7 +14,3 @@ class Space:
     def __repr__(self):
         return f"Space({self.id}, {self.name}, {self.description}, {self.start_date}, {self.end_date}, {self.price}, {self.user_id})"
     
-
-
-    # [Space(1, space1, description1, 2030-01-01, 2030-02-01, 1000, 1), Space(2, space2, description2, 2031-02-02, 2031-02-28, 15000, 1), Space(3, space3, description3, 2030-01-15, 2030-02-15, 2000, 2), Space(4, space4, description4, 2030-03-04, 2030-03-14, 70000, 2)]
-    # [Space(1, space1, description1, 2030-01-01, 2030-02-01, 1000, 1), Space(2, space2, description2, 2031-02-02, 2031-02-28, 15000, 1), Space(3, space3, description3, 2030-01-15, 2030-02-15, 2000, 2), Space(4, space4, description4, 2030-03-04, 2030-03-14, 70000, 2)]
--- lib/space_repository.py
+++ lib/space_repository.py
@@ -9,7 +9,17 @@ class SpaceRepository:
         spaces = []
         for row in rows:
             item = Space(row["id"], row["name"], 
-                         row["description"], row["start_date"], 
-                         row["end_date"], row["price"], row["user_id"])
+                        row["description"], row["start_date"], 
+                        row["end_date"], row["price"], row["user_id"])
             spaces.append(item)
         return spaces
+    
+    def add_space(self, space):
+        if space.end_date < space.start_date:
+            raise Exception("End date cannot be earlier than start date")
+        else: 
+            self._connection.execute(
+                'INSERT INTO spaces (name, description,start_date, end_date, price, user_id) VALUES (%s, %s, %s, %s, %s, %s)',
+                [space.name, space.description, space.start_date, space.end_date, space.price, space.user_id]
+                )
+        return None
